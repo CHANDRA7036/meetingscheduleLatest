4916386eb19cbd46183f5bd02018107180764897
CHANDRA7036 <chandra.iedamakanti@nexgdata.com>
2020-05-26 19:17:42 +0530

    schedule page displying form

--- src/app/components/calendar/calendar.component.ts
+++ src/app/components/calendar/calendar.component.ts
@@ -31,16 +31,41 @@ export class CalendarComponent implements OnInit {
   constructor() { 
 
     this.timeSlots={
-        slotButtons:[
-          '9.00AM','9.15AM','9.30AM','9.45AM','10.00AM','10.15AM','10.30AM',
-          '10.45AM','11.00AM','11.15AM','11.30AM','11.45AM','12.00PM','12.15PM',
-          '12.30PM','12.45PM','1.00PM','1.15PM','1.30PM','1.45PM','2.00PM','2.15PM',
-          '2.30PM','2.45PM','3.00PM','3.15PM','3.30PM','3.45PM','4.00PM','4.15PM','4.30PM'
-        ],
-        confirmBtnDisplay:[false,false,false,false,false,false,false,false,false,false,
-          false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false]
-    }
 
+          slotButtons:[
+            {slotTime:'9.00AM' , isActive:false},
+            {slotTime:'9.15AM' , isActive:false},
+            {slotTime:'9.30AM' , isActive:false},
+            {slotTime:'9.45AM' , isActive:false},
+            {slotTime:'10.00AM' , isActive:false},
+            {slotTime:'10.15AM' , isActive:false},
+            {slotTime:'10.30AM' , isActive:false},
+            {slotTime:'10.45AM' , isActive:false},
+            {slotTime:'11.00AM' , isActive:false},
+            {slotTime:'11.15AM' , isActive:false},
+            {slotTime:'11.30AM' , isActive:false},
+            {slotTime:'11.45AM' , isActive:false},
+            {slotTime:'12.00PM' , isActive:false},
+            {slotTime:'12.10PM' , isActive:false},
+            {slotTime:'12.30PM' , isActive:false},
+            {slotTime:'12.45PM' , isActive:false},
+            {slotTime:'1.00PM' , isActive:false},
+            {slotTime:'1.15PM' , isActive:false},
+            {slotTime:'1.30PM' , isActive:false},
+            {slotTime:'1.45PM' , isActive:false},
+            {slotTime:'2.00PM' , isActive:false},
+            {slotTime:'2.15PM' , isActive:false},
+            {slotTime:'2.30PM' , isActive:false},
+            {slotTime:'2.45PM' , isActive:false},
+            {slotTime:'3.00PM' , isActive:false},
+            {slotTime:'3.15PM' , isActive:false},
+            {slotTime:'3.30PM' , isActive:false},
+            {slotTime:'3.45PM' , isActive:false},
+            {slotTime:'4.00PM' , isActive:false},
+            {slotTime:'4.15PM' , isActive:false},
+            {slotTime:'4.30PM' , isActive:false}           
+          ]
+    }
   }
 
   onValueChange(value: Date): void {
@@ -50,6 +75,38 @@ export class CalendarComponent implements OnInit {
     console.log(`Current value: ${change.date}`);
     console.log(`Current mode: ${change.mode}`);
   }
+
+meetingDuration:any
+
+  calculateTime($event){
+    this.meetingDuration=$event.slotTime;
+  }
+
+isDetailsFormdisplay:boolean;
+onClickConfirmButtonInCalendar($event){
+  this.isDetailsFormdisplay=$event;
+}
+//   disabledDate = (current) =>{         
+//     // Can not select days after today and before start Date
+//     const start = moment('2020-01-01','YYYY-MM-DD');        
+//     return  current< start || current>moment();
+// }
+
+  // disabledDate={(current)=>
+  //   return [moment().add(-1,'days')]
+  // }
+
+
+//   slotTimeUpdate:any;
+
+//   changeSlotSelectedValue(ButtonSlots){
+//   this.slotTimeUpdate=this.timeSlots.slotButtons.find(this.findSlot,[ButtonSlots.slotTime]);
+//   this.slotTimeUpdate.isActive=true;
+//   }
+
+//   findSlot(ButtonSlots){
+//     return ButtonSlots.isActive === this[0];
+// }
 
 //  isShow=true;
 //  onClickMeetingDiv(){
@@ -84,6 +141,7 @@ guestEmailsClick(){
 // time = document.getElementsByClassName('active');
 
 selectedValue=new Date();
+
 
 
 }
--- src/app/components/slots/slots.component.ts
+++ src/app/components/slots/slots.component.ts
@@ -1,5 +1,5 @@
-import { Component, OnInit, Input, Output } from '@angular/core';
-import { EventEmitter } from 'protractor';
+import { Component, OnInit, Input, Output ,EventEmitter} from '@angular/core';
+
 
 @Component({
   selector: 'app-slots',
@@ -9,34 +9,49 @@ import { EventEmitter } from 'protractor';
 export class SlotsComponent implements OnInit {
 
   @Input() timeSlots:string;
-  @Input() isSlotSelected:boolean;
-  //@Output() isSlotSelectedAfterChange:EventEmitter=new EventEmitter();
-  //@Output() isConfirmClicked:EventEmitter=new EventEmitter();
+  @Input() isSlotActive:boolean;
+  @Input() allISActives:any;
+  @Output() isSlotSelectedAfterChange:EventEmitter<any>=new EventEmitter();
+  @Output() selectedTime:EventEmitter<any>=new EventEmitter();
+  @Output() formDisplayToggle:EventEmitter<any>=new EventEmitter();
   ngOnInit(): void {
   }
 
   constructor() {    
   }
 
-  isDisplay=false;
-  onClickCalendar(){
-    this.isDisplay=true;
-  }
-  
-//   isSlotDisplay=false;
-  onClickSlot(){
-    this.isSlotSelected=!this.isSlotSelected;
-    //this.isSlotSelectedAfterChange.emit(`${this.isSlotSelected}`);
-  }
-  
-
-  // confirmClick(){
-  //   this.isConfirmClicked.emit('1');
+  // isDisplay=false;
+  // onClickCalendar(){
+  //   this.isDisplay=true;
   // }
 
+dateSlotSelected(){
+  this.selectedTime.emit({slotTime:this.timeSlots});  
+}
+
+  onClickSlot(){
+    this.isSlotActive=!this.isSlotActive;
+
+    for (let index = 0; index < this.allISActives.length; index++) {
+      if(this.allISActives[index].slotTime==this.timeSlots){
+        this.allISActives[index].isActive=this.isSlotActive;
+        //alert(this.allISActives[index].isActive);
+      }else{
+        this.allISActives[index].isActive=false;
+      }
+    }
+    this.isSlotSelectedAfterChange.emit({isActive:this.allISActives}); 
+  }  
+  
 isGuestEmailsDisplay=false;
 guestEmailsClick(){
   this.isGuestEmailsDisplay=!this.isGuestEmailsDisplay;
+}
+
+isFormDisplay=false;
+onClickConfirmButton(){
+  this.isFormDisplay=!this.isFormDisplay;
+  this.formDisplayToggle.emit(this.isFormDisplay);
 }
 
 }
